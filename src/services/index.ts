cb1adbf268336cda6c96f92e9e31b87a4bb55a6f
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-05-01 15:12:24 -0400

    refactor: move prisma client generated files to node_modules

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,16 @@
+import bcrypt from 'bcrypt';
+
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+    query: {
+      user: {
+        $allOperations({ operation, args, query }) {
+          if (['create', 'update'].includes(operation) && args.data['password']) {
+            args.data['password'] = bcrypt.hashSync(args.data['password'], 10)
+          }
+          return query(args)
+        }
+      }
+    }
+  });
