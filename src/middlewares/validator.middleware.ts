00e9d49e1290e9daa50986ce41404d43f2009f0e
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-06-14 21:07:58 -0400

    refactor: update to zod v4 for filtering unspecified properties in schemas

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,14 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
-import { AnyZodObject } from "zod";
+import { ZodObject } from "zod/v4";
 
-export const validator = (schema: AnyZodObject) =>
+export const validator = (schema: ZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await schema.parseAsync({
+      const parsed = await schema.parseAsync({
         body: req.body,
         query: req.query,
         params: req.params,
       });
+
+      req.body = parsed.body;
       next();
     } catch (error) {
       res.status(422).json(error);
