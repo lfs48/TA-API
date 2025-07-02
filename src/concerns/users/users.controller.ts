22982e4f3fe8e2de4664fdf60c714944365375ce
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-07-02 17:03:42 -0400

    fix: key user response data under user

--- src/concerns/users/users.controller.ts
+++ src/concerns/users/users.controller.ts
@@ -17,7 +17,7 @@ export const getUser = async (req: Request, res: Response) => {
             res.status(404).json({ message: 'User not found' });
             return;
         }
-        res.status(200).json( whitelistUserFields(user) );
+        res.status(200).json({ user: whitelistUserFields(user) });
         return;
     } catch (error) {
         res.status(500).json({ message: 'Internal Server Error' });
