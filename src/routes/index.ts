3df75bb733864b1919c18400c29bd8613346638c
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-07-06 17:42:42 -0400

    refactor: remove example routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-import exampleController from '@/controllers';
 import { authenticator, logger } from '@/middlewares';
 import authRouter from '@/concerns/auth/auth.routes';
 import userRouter from '@/concerns/users/user.routes';
@@ -12,10 +11,6 @@ const router = Router();
 router.use([
     logger,
 ]);
-
-//Example routes
-router.get('/example', authenticator, exampleController.getExample);
-router.post('/example', exampleController.createExample);
 
 //Inject routes
 router.use(authRouter);
