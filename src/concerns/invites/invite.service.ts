73f5a45cc8e18061d2a1d64065f3e2bef353883e
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-07-08 21:14:58 -0400

    refactor: define invites relation constant

--- src/concerns/invites/invite.service.ts
+++ src/concerns/invites/invite.service.ts
@@ -8,20 +8,12 @@ import {
 
 export const findInviteById = async (id: string):Promise<InviteWithRelations | null> => await prisma.invite.findUnique({
     where: { id: id },
-    include: {
-        invitee: true,
-        inviter: true,
-        game: true,
-    }
+    include: allInviteRelations,
 })
 
 export const createInvite = async (inviteData:InviteData):Promise<InviteWithRelations> => await prisma.invite.create({
     data: { ...inviteData },
-    include: {
-        invitee: true,
-        inviter: true,
-        game: true,
-    }
+    include: allInviteRelations,
 });
 
 export const rejectInvite = async (
@@ -30,11 +22,7 @@ export const rejectInvite = async (
     const invite = await prisma.invite.update({
         where: { id },
         data: { status: "REJECTED" },
-        include: {
-            invitee: true,
-            inviter: true,
-            game: true,
-        },
+        include: allInviteRelations,
     });
 
     return invite;
@@ -45,11 +33,7 @@ export const acceptInvite = async (id: string): Promise<InviteWithRelations> => 
         const invite = await tx.invite.update({
             where: { id },
             data: { status: "ACCEPTED" },
-            include: {
-                invitee: true,
-                inviter: true,
-                game: true,
-            },
+            include: allInviteRelations,
         });
 
         await tx.game.update({
@@ -67,20 +51,12 @@ export const acceptInvite = async (id: string): Promise<InviteWithRelations> => 
 
 export const findGameInvites = async(gameId:string): Promise<InviteWithRelations[]> => await prisma.invite.findMany({
     where: { gameId },
-    include: {
-        invitee: true,
-        inviter: true,
-        game: true,
-    }
+    include: allInviteRelations,
 });
 
 export const findReceivedInvites = async(inviteeId:string): Promise<InviteWithRelations[]> => await prisma.invite.findMany({
     where: { inviteeId },
-    include: {
-        invitee: true,
-        inviter: true,
-        game: true,
-    }
+    include: allInviteRelations,
 });
 
 export const findPendingGameInvite = async(inviteeId: string, gameId:string): Promise<Invite | null> => {
@@ -90,7 +66,14 @@ export const findPendingGameInvite = async(inviteeId: string, gameId:string): Pr
             gameId: gameId,
             status: "PENDING",
         },
+        include: allInviteRelations,
     });
 
     return invite;
 }
+
+const allInviteRelations = {
+    invitee: true,
+    inviter: true,
+    game: true,
+};
